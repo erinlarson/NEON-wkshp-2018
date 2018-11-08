8d96c1b74f1d1435ae7276ba895b2d8f7bcb76e7
ernlarson <ern.larson@gmail.com>
2018-11-07 20:31:42 -0900

    downloading packages

--- NEON Data Workshop.R
+++ NEON Data Workshop.R
@@ -2,3 +2,12 @@
 ## Nov 8-9, 2018
 
 ## Downloading and attaching packages needed for NEON workshop
+install.packages("raster")
+install.package("neonUtilities")
+install.packages("devtools")
+
+
+source("http://bioconductor.org/biocLite.R")
+biocLite("rhdf5")
+library(devtools)
+install_github("NEONScience/NEON-geolocation/geoNEON")
