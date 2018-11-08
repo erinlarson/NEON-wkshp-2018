e0f1d7b3a10f838e3139fe1da5c599291466ff60
ernlarson <ern.larson@gmail.com>
2018-11-08 14:10:36 -0900

    pulling down bird data from API

--- NEON API Tutorial.R
+++ NEON API Tutorial.R
@@ -4,5 +4,36 @@
 # Accessing NEON API
 # Tutorial also available at https://www.neonscience.org/neon-api-usage
 
+# Getting set up
+# Uncomment these to install packages if you don't have them!
+# install.packages("httr")
+# install.packages("jsonlite")
+# install.packages("downloader")
+library(httr)
+library(jsonlite)
+library(downloader)
 
+# writing an API request
+req <- httr::GET("http://data.neonscience.org/api/v0/products/DP1.10003.001")
+req
 
+req.content <- httr::content(req, as="parsed")
+req.content
+
+req.text <- content(req, as="text")
+
+available<-fromJSON(req.text, simplifyDataFrame=T, flatten=T)
+available
+
+available$data$siteCodes
+
+# identifying where bird data is available
+bird.urls <- unlist(available$data$siteCodes$availableDataUrls)
+bird.urls
+
+# get data availability for WOOD July 2015
+brd <- httr::GET(bird.urls[grep("WOOD/2015-07", bird.urls)])
+brd.files <- fromJSON(content(brd, as="text"))
+
+# view just the available data files
+brd.files$data$files
