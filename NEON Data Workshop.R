76273301fe1cd75c35a6d2ed585eb5c39854a5b8
ernlarson <ern.larson@gmail.com>
2018-11-08 08:29:29 -0900

    Getting started Day 1

--- NEON Data Workshop.R
+++ NEON Data Workshop.R
@@ -2,18 +2,23 @@
 ## Nov 8-9, 2018
 
 ## Downloading and attaching packages needed for NEON workshop
-install.packages("raster")
-install.package("neonUtilities")
-install.packages("devtools")
+# uncomment to install
+#install.packages("raster")
+#install.packages("neonUtilities")
+#install.packages("devtools")
 
 
-source("http://bioconductor.org/biocLite.R")
-biocLite("rhdf5")
-library(devtools)
-install_github("NEONScience/NEON-geolocation/geoNEON")
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("rhdf5")
+#library(devtools)
+#install_github("NEONScience/NEON-geolocation/geoNEON")
 
+##NEON WORKSHOP DAY 1
 require(raster)
 require(neonUtilities)
 require(devtools)
 require(rhdf5)
 require(geoNEON)
+
+options(stringsAsFactors=F)
+
